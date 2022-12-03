728664d86ee309b9e05e8529b579686205c217cc
Stephen <skairu09@gmail.com>
2022-12-03 22:39:44 +0300

    Completed assignment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,13 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(username: params[:username])
-    session[:user_id] = user.id
+    session[:user_id] = user
+    if session[:user]
     render json: user
+    else 
+      render json: {error: "You have to login first."}, status: :unauthorized
   end
+end
 
   def destroy
     session.delete :user_id
